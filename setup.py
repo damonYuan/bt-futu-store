60593403034c097be3600450ed0b1b883840fcf2
damonYuan <yzmhit@gmail.com>
2022-07-22 13:28:01 +0800

    update

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 from setuptools import setup
+import os
+REQUIREMENTS = [i.strip() for i in open("requirements.txt").readlines()]
 
 setup(
    name='bt_futu_store',
    version='1.0',
    description='Futu API store for backtrader',
+   license='MIT',
    url='',
    author='Damon Yuan',
    author_email='damon.yuan.dev@gmail.com',
-   license='MIT',
    packages=['btfutu'],
-   install_requires=['backtrader', 'futu-api'],
+   install_requires=REQUIREMENTS
 )
